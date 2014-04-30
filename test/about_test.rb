8da0c42fd325b56945e73edbbd4de3cb9a05e54a
Vlada Karan <kejkzz@gmail.com>
2014-04-30 19:32:53 +0200

    Moved tests to their folder

--- test/about_test.rb
+++ test/about_test.rb
@@ -3,22 +3,22 @@ require 'minitest/autorun'
 
 class BlenderTesting < Minitest::Test
 
- def setup
+  def setup
     @b = Watir::Browser.new :chrome
     @b.window.resize_to(1024,800)
     @b.goto 'http://www.blender.org/' 
   end
   
- def test_about_page
+  def test_about_page
     @b.li(id: 'menu-item-164').click
     assert_equal(@b.title.lstrip, "About - blender.org - Home of the Blender project - Free and Open 3D Creation Software")
     assert_equal(@b.url, "http://www.blender.org/about/")
     assert(@b.div(class: "span6", index: 0).h1.text.include? "The organization")
-	end 
+  end 
   
     def teardown
-    @b.screenshot.save 'output.png' if !passed?
-    @b.close
-  end
+      @b.screenshot.save 'output.png' if !passed?
+      @b.close
+    end
   
 end
