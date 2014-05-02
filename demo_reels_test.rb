d9a2c3f5c6252e0d5c510206d0bd806af247a28b
srdjanbilic <srdjanbilic@gmail.com>
2014-05-02 13:30:19 +0200

    flash

--- demo_reels_test.rb
+++ demo_reels_test.rb
@@ -14,7 +14,7 @@ class BlenderTesting < Minitest::Test
     @b.li(class: "page_item page-item-15984", index: 0).click 
     assert_equal(@b.title.lstrip, "Demo reels - blender.org - Home of the Blender project - Free and Open 3D Creation Software")
     assert_equal(@b.url, "http://www.blender.org/features/demo-reels/")
-    #@b.div(id: "post-15984", index: 0).click 
+    @b.link(rel: "canonical", index: 0).click 
 	end 
   
     def teardown
